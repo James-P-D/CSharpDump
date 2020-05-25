bd465e7f7a5c5a3352bc81e6fdaf36020ac1b169
James Dorrington <jdorrington@hotmail.com>
2020-05-25 12:19:43 +0100

    Adding very minor project for displaying unicode characters in Windows Console Application. (A billion times easier that Java :D)

---
+++ src/SpecialCharacters/SpecialCharacters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SpecialCharacters
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.OutputEncoding = System.Text.Encoding.Unicode;
+      Console.WriteLine("\u03B8");
+      Console.WriteLine("ΛλᴧⲖⲗ");
+      Console.WriteLine("\u039B");
+      Console.ReadLine();
+
+    }
+  }
+}
